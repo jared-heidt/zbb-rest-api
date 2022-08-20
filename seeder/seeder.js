93a79cd8d5cf23bc742c167822d70b09717d3583
Jared Heidt <jared14heidt@gmail.com>
2022-08-20 01:20:09 -0400

    completed seeder

---
+++ seeder/seeder.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const colors = require('colors');
+const mongoose = require('mongoose');
+const connectDB = require('./src/db/connect')
+require('dotenv').config();
+
+// Load models
+const Album = require('./src/model/Album');
+
+connectDB(process.env.MONGO_URI)
+
+// Read JSON files
+const album = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/albums.json`, 'utf-8')
+);
+
+// Import into DB
+const importData = async () => {
+  try {
+    await Album.create(album);
+
+    console.log('Data Imported...'.green.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// Delete data
+const deleteData = async () => {
+  try {
+    await Album.deleteMany();
+    console.log('Data Destroyed...'.red.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+if (process.argv[2] === '-i') {
+  importData();
+} else if (process.argv[2] === '-d') {
+  deleteData();
+}
